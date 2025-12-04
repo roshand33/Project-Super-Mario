d59f67bd9560081c442760d0fa1725ff75e23e80
Roshan Vijay Dobe <roshandobe48@gmail.com>
2025-12-04 18:52:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "super-mario-tf-kubernetes-project-new" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
-    profile = "roshan"
   }
 }
